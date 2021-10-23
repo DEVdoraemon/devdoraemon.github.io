b3b73214b403c2d72999e750ada0bb62bf807d59
Rahul Awasthi <awasthir@pm.me>
2021-10-23 23:58:22 +0530

    Updates

---
+++ precache-manifest.2fdb0913b22a63a5c9c90e2a25710760.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e1bf0f1730451fa65efeee9e8e4bf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ea99afb210e2d943408",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "e4bc56c3bb836fe14e27",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "53545c0a6623d3408052",
+    "url": "/static/css/main.247cf513.chunk.css"
+  },
+  {
+    "revision": "2259d2a86ba25c6a93f9",
+    "url": "/static/js/2.06ef12a7.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.06ef12a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea99afb210e2d943408",
+    "url": "/static/js/3.08f25c2a.chunk.js"
+  },
+  {
+    "revision": "e4bc56c3bb836fe14e27",
+    "url": "/static/js/4.80d08696.chunk.js"
+  },
+  {
+    "revision": "53545c0a6623d3408052",
+    "url": "/static/js/main.e88927af.chunk.js"
+  },
+  {
+    "revision": "6a746af3b16ff80e8c5f",
+    "url": "/static/js/runtime-main.06dcb736.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "136ac79f16da7c774b8e87eae501a907",
+    "url": "/static/media/Sanskritam_logo.136ac79f.png"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "31723c076f70ae362bff0aed26d82dfe",
+    "url": "/static/media/psit_logo.31723c07.png"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
